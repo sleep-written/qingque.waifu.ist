c02c78dd4d39f2067bd71ed58482036cc4ec7990
sleep-written <sleep.written0000@gmail.com>
2024-01-29 21:00:28 -0300

    Changes: - Added a banner in main page.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { PagesModule } from './pages/pages.module.js';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +17,22 @@ import { PagesModule } from './pages/pages.module.js';
     RouterModule,
     PagesModule,
 
+    MatSidenavModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  opened = false;
+
+  constructor(
+    private _title: Title,
+  ) {}
+
+  ngOnInit(): void {
+    this._title.setTitle('Qingque\'s Room');
+  }
 }
